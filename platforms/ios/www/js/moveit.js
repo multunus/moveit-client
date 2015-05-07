75fce3abbfba9ec9289ca7f4a7f7453f280d5cb2
Muraleekrishna Gopinathan <muraleekrishna.g@multunus.com>
2015-05-07 11:37:30 +0530

    Add timeline activity icon

--- platforms/ios/www/js/moveit.js
+++ platforms/ios/www/js/moveit.js
@@ -15,3 +15,54 @@ var enableSaveButtonWith = function(form, text){
   saveBtn.disabled = false;
   saveBtn.textContent = text;
 };
+
+var getParameterByName = function(name, location) {
+  var query_name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + query_name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
+
+var UserInteraction = {
+  action: function(fromEmail, toEmail, draggable, action, name){
+
+    if(fromEmail != toEmail &&  action != 'none'){
+      draggable.animate({
+        left: '+=400px'
+      }, 250);
+
+      draggable.siblings('.interaction-message.' + action + 'ing').removeClass('hidden');
+      draggable.hide();
+
+      var data = {
+        from_email_id : fromEmail,
+        to_email_id : toEmail
+      };
+
+      $.ajax({
+        dataType: 'json',
+        url: settings.getSetting("userApiUrl") + action,
+        type: 'POST',
+        data: data,
+        success: function(data, textStatus, jqXHR) {
+          console.log(action + "ing....");
+          Materialize.toast("You " + action + "'d " + name, 2000);
+          draggable.unbind("swiperight");
+          draggable.addClass('none');
+        },
+        error: function() {
+          Materialize.toast("Please check your internet connection.", 3000);
+        }
+      });
+
+      setTimeout(function(){
+        draggable.show();
+        draggable.animate({
+          left: '-=400px'
+        }, 250);
+        draggable.siblings('.interaction-message.' + action + 'ing').addClass('hidden');
+      }, 500);
+    }
+  }
+};
+
--- platforms/ios/www/js/settings.js
+++ platforms/ios/www/js/settings.js
@@ -1,5 +1,18 @@
 var settings = {
-  server:{
-    baseUrl: "http://mov3it.herokuapp.com/"
+  test:{
+    baseUrl: "https://pink-server.herokuapp.com/",
+    apiUrl: "http://pink-server.herokuapp.com/api/",
+    userApiUrl: "http://pink-server.herokuapp.com/api/user/"
+  },
+  production:{
+    baseUrl: "https://mov3it.herokuapp.com/",
+    apiUrl: "http://mov3it.herokuapp.com/api/",
+    userApiUrl: "http://mov3it.herokuapp.com/api/user/"
+  },
+
+  getSetting: function(setting){
+    var settings = JSON.parse(localStorage.getItem("settings")) || {};
+    var mode = settings.mode || "production";
+    return this[mode][setting];
   }
 };
